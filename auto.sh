0968fef665f0568bc10156501263f669fd3456f3
Sajal1005 <nandasajal.208@gmail.com>
2023-04-11 20:49:37 +0530

    Auto-commit at 2023-04-11 20:49:37

--- auto.sh
+++ auto.sh
@@ -16,7 +16,6 @@ git commit -m "Auto-commit at $(date "+%Y-%m-%d %H:%M:%S")"
 
 # Push the changes to the GitHub repository
 git push $REPO_URL $BRANCH_NAME
-printf 'Sajal1005\n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\n'
 
 
 # Print a message to indicate that the push was successful
