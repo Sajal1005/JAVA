9228519b5d68a55305b6905192c159b861ca0a83
Sajal1005 <nandasajal.208@gmail.com>
2023-04-11 19:53:09 +0530

    Auto-commit at 2023-04-11 19:53:08

--- auto.sh
+++ auto.sh
@@ -3,6 +3,8 @@
 # Set the GitHub repository URL and branch name
 REPO_URL="https://github.com/Sajal1005/JAVA.git"
 BRANCH_NAME="main"
+USERNAME="Sajal1005"
+TOKEN="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"
 
 # Change directory to the folder that you want to push changes from
 cd /mnt/c/Users/Nightriders/Desktop/JAVA 
@@ -14,7 +16,8 @@ git add .
 git commit -m "Auto-commit at $(date "+%Y-%m-%d %H:%M:%S")"
 
 # Push the changes to the GitHub repository
-git push $REPO_URL $BRANCH_NAME
+git push $REPO_URL $BRANCH_NAME --quiet --force --set-upstream "https://$USERNAME:$TOKEN@github.com/yourusername/repo.git"
+
 
 # Print a message to indicate that the push was successful
 echo "Changes pushed to GitHub repository: $REPO_URL"
