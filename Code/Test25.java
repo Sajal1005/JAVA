67e39f16d48b08ea1b33b87c540dc657e638609a
Sajal1005 <nandasajal.208@gmail.com>
2023-04-13 21:30:03 +0530

    Auto-commit at 2023-04-13 21:30:02

--- Code/Test25.java
+++ Code/Test25.java
@@ -3,11 +3,9 @@ import java.util.*;
 public class Test25 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for(int i=0;i<=n;i++){
-            if(i%2==0){
-                System.out.println(i);
-            }
+        String s= "Sajal";
+        for(int i=0;i<s.length();i++){
+            System.out.print(s.charAt(i)+" ");
         }
     }
 }
