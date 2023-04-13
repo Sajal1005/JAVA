411df6b2c0f30eb5afb33d2ff637ff2adf4e00e4
Sajal1005 <nandasajal.208@gmail.com>
2023-04-13 19:00:03 +0530

    Auto-commit at 2023-04-13 19:00:02

--- Code/Patterns/Patterns.java
+++ Code/Patterns/Patterns.java
@@ -475,7 +475,32 @@ public class Patterns {
 //        *   *
 //       *   *
 //      *****
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<n;j++){
+//                if(i==0 || j==0 || i==n-1 || j==n-1){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
-        
+//        32.
+//        E
+//        D E
+//        C D E
+//        B C D E
+//        A B C D E
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                char c = (char) ((n-i+j)+64);
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
