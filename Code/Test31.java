b226be12acc6564fb283afe3b0ecf2f081311341
Sajal1005 <nandasajal.208@gmail.com>
2023-04-15 06:30:03 +0530

    Auto-commit at 2023-04-15 06:30:02

---
+++ Code/Test31.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Test31{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" X "+i+" = "+(n*i));
+        }
+    }
+}
