db647e2c9c9739b8e1e3cf476737ed8cff23ac37
Sajal1005 <nandasajal.208@gmail.com>
2023-04-15 08:10:03 +0530

    Auto-commit at 2023-04-15 08:10:02

--- Code/Array/Reverse.java
+++ Code/Array/Reverse.java
@@ -14,9 +14,15 @@ public class Reverse {
 //        }
 
 //        Reversing
-        int i=0,j=n-1;
-        while(i<j){
-            
-        }
+//        int i=0,j=n-1;
+//        while(i<j){
+//            int temp=arr[i];
+//            arr[i]=arr[j];
+//            arr[j]=temp;
+//            i++;j--;
+//        }
+//        for(i=0;i<n;i++){
+//            System.out.print(arr[i]+" ");
+//        }
     }
 }
