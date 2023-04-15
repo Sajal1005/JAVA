ab271ff38c5d299b427fc4c74cb0b81ab034fed7
Sajal1005 <nandasajal.208@gmail.com>
2023-04-15 06:40:03 +0530

    Auto-commit at 2023-04-15 06:40:02

--- Code/Test31.java
+++ Code/Test31.java
@@ -4,8 +4,30 @@ class Test31{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for(int i=1;i<=10;i++){
-            System.out.println(n+" X "+i+" = "+(n*i));
+//        for(int i=1;i<=10;i++){
+//            System.out.println(n+" X "+i+" = "+(n*i));
+//        }
+//        for(int i=1;i<=10;i++){
+//            for(int j=1;j<=n;j++){
+//                System.out.print(j+"X"+i+"="+(j*i)+", ");
+//            }
+//            System.out.println();
+//        }
+//        int k=1;
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(k++ +" ");
+//            }
+//            System.out.println();
+//        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*i+1);j++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
 }
