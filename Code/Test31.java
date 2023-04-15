594e80fdda3c508d0186c8ecbb4b5aafcb202b8d
Sajal1005 <nandasajal.208@gmail.com>
2023-04-15 07:00:02 +0530

    Auto-commit at 2023-04-15 07:00:01

--- Code/Test31.java
+++ Code/Test31.java
@@ -36,14 +36,37 @@ class Test31{
 //        }
 //        System.out.printf("%,.6f",ans);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<(2*i+1);j++){
-                System.out.print("*");
-            }
-            System.out.println();
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<(2*i+1);j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        int k=9,ans=0;
+//        for(int i=1;i<=n;i++){
+//            ans+=k;
+//            k*=10;
+//            k+=9;
+//        }
+//        System.out.println(ans);
+        int num=n,count=0,ans=0;
+        while(n!=0){
+            count++;
+            n/=10;
+        }
+        n=num;
+        while(n!=0){
+            int r=n%10;
+            ans+=(Math.pow(r,count));
+            n/=10;
+        }
+        if(ans==num){
+            System.out.println("Yes");
+        }else{
+            System.out.println("Np");
         }
     }
 }
