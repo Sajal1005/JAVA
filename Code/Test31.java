ee337fad1391769170e0156598fbe157f4012dde
Sajal1005 <nandasajal.208@gmail.com>
2023-04-15 07:40:03 +0530

    Auto-commit at 2023-04-15 07:40:02

--- Code/Test31.java
+++ Code/Test31.java
@@ -68,23 +68,53 @@ class Test31{
 //        }else{
 //            System.out.println("Np");
 //        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<(2*i+1);j++){
-                System.out.print("*");
-            }
-            System.out.println();
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<(2*i+1);j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n-1;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<(2*n-3-(2*i));j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                char c = (char)(j+65);
+//                System.out.print(c+" ");
+//            }
+//            for(int j=0;j<i;j++){
+//                char c = (char)(i+64-j);
+//                System.out.print(c+" ");
+//            }
+//            System.out.println();
+//        }
+//        int count=0,ans=0;
+//        while(n!=0){
+//            int r=n%10;
+//            ans+=(Math.pow(2,count++)*r);
+//            n/=10;
+//        }
+//        System.out.print(ans);
+        ArrayList<Integer> arr = new ArrayList<>();
+        while(n!=0){
+            int r=n%2;
+            arr.add(r);
+            n/=2;
         }
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<i+1;j++){
-                System.out.print(" ");
-            }
-            for(int j=0;j<(2*n-3-(2*i));j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        for(int i=arr.size()-1;i>=0;i--){
+            System.out.print(arr.get(i));
         }
     }
 }
