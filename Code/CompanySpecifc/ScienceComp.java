488d842dc09fc082e7409930b39215fd9d003a4a
Sajal1005 <nandasajal.208@gmail.com>
2023-04-20 15:50:02 +0530

    Auto-commit at 2023-04-20 15:50:01

--- Code/CompanySpecifc/ScienceComp.java
+++ Code/CompanySpecifc/ScienceComp.java
@@ -1,2 +1,20 @@
-package CompanySpecifc;public class ScienceComp {
+package CompanySpecifc;
+import java.util.*;
+
+public class ScienceComp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int k = in.nextInt();
+        Arrays.sort(arr);
+        for(int i=n-1;i>=0;i--){
+            if((k--)>0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
 }
