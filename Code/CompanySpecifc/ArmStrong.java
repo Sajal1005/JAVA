5955b6a3257b19f04eb3c93c889220207b7432f3
Sajal1005 <nandasajal.208@gmail.com>
2023-04-20 10:20:03 +0530

    Auto-commit at 2023-04-20 10:20:02

--- Code/CompanySpecifc/ArmStrong.java
+++ Code/CompanySpecifc/ArmStrong.java
@@ -1,2 +1,29 @@
-package CompanySpecifc;public class ArmStrong {
+package CompanySpecifc;
+import java.util.*;
+
+public class ArmStrong {
+    public static boolean arm(int n){
+        int count=0,num=n,ans=n;
+        while(n!=0){
+            count++;
+            n/=10;
+        }
+        n=num;
+        num=0;
+        while(n!=0){
+            int r=n%10;
+            num+=(Math.pow(r,count));
+            n/=10;
+        }
+        if(num==ans){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n =in.nextInt();
+
+    }
 }
