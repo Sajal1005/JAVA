dbc888430e124bb53c1b32ae08dde180e20fd879
Sajal1005 <nandasajal.208@gmail.com>
2023-04-20 15:30:03 +0530

    Auto-commit at 2023-04-20 15:30:02

--- Code/CompanySpecifc/AddNumToString.java
+++ Code/CompanySpecifc/AddNumToString.java
@@ -2,4 +2,15 @@ package CompanySpecifc;
 import java.util.*;
 
 public class AddNumToString {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n = in.nextInt();
+        String s = in.next();
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            char c = (char)(s.charAt(i)+n);
+            ans+=c;
+        }
+        System.out.println(ans);
+    }
 }
