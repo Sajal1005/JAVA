b3c61c0fa2a864a24aa1a0dd1a5b690da6b00ea7
Sajal1005 <nandasajal.208@gmail.com>
2023-04-21 11:40:04 +0530

    Auto-commit at 2023-04-21 11:40:02

--- Code/OOPS/Tets2.java
+++ Code/OOPS/Tets2.java
@@ -1,2 +1,16 @@
-package OOPS;public class Tets2 {
+package OOPS;
+
+class Parent2{
+    String name="Sajal";
+    Parent2(){
+        System.out.println("Constructor called");
+    }
+}
+class Child2 extends Parent2{
+    Child2(){
+        super();
+    }
+    public static void main(String[] args) {
+        Child2 obj = new Child2();
+    }
 }
