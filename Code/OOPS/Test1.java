797ee5448aa15a91267bf0588d9f2917f7c02105
Sajal1005 <nandasajal.208@gmail.com>
2023-04-21 11:10:04 +0530

    Auto-commit at 2023-04-21 11:10:02

--- Code/OOPS/Test1.java
+++ Code/OOPS/Test1.java
@@ -1,14 +1,17 @@
 package OOPS;
 
 class Parent{
-    String name="Parent";
+     String name="Parent";
 }
 class Child extends Parent{
     String name="Child";
-
-    public static void main(String[] args) {
+    public  void hello()
+    {
+        System.out.println(super.name);
+    }
+    public  void main(String[] args) {
         Child obj = new Child();
         System.out.println(obj.name);
-        System.out.println(super.name);
+        hello();
     }
 }
