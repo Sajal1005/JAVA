b68e90029d57a41ab112265f7faf7f5fa5a9030b
Sajal1005 <nandasajal.208@gmail.com>
2023-05-04 20:10:03 +0530

    Auto-commit at 2023-05-04 20:10:02

--- Code/String/IndexOfSubStr.java
+++ Code/String/IndexOfSubStr.java
@@ -1,2 +1,36 @@
-package String;public class IndexOfSubStr {
+package String;
+import java.util.*;
+
+public class IndexOfSubStr {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        String b= in.nextLine();
+        int i=0,j=0,flag=0;
+        while(i<a.length()){
+            int k=i;
+            if(a.charAt(i)==b.charAt(j)){
+                while(i<a.length() && j<b.length() && a.charAt(i)==b.charAt(j)){
+                    i++;
+                    j++;
+                }
+                if(j==b.length()){
+                    System.out.println(k);
+                    flag=1;
+                    break;
+                }else{
+                    i=k+1;
+                    j=0;
+                }
+            }else{
+                i++;
+            }
+            if(flag==1){
+                break;
+            }
+        }
+        if(flag==0){
+            System.out.println(-1);
+        }
+    }
 }
