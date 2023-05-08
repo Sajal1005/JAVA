40927c3c6009ab1c40c0a97205e383834797a3fe
Sajal1005 <nandasajal.208@gmail.com>
2023-05-08 14:43:07 +0530

    Auto-commit at 2023-05-08 14:43:06

--- Code/String/NumberOfWords.java
+++ Code/String/NumberOfWords.java
@@ -1,5 +1,15 @@
 package String;
 
 public class NumberOfWords {
-    
+    public int countWords(String s) {
+        s=s.trim();
+        if(s.length()==0){return 0;}
+        int count=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)==' ' && s.charAt(i+1)!=' '){
+                count++;
+            }
+        }
+        return count+1;
+    }
 }
