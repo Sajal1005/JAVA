ba0494d8bd84c953af75340c001ca5e8af046fed
Sajal1005 <nandasajal.208@gmail.com>
2023-05-15 09:59:02 +0530

    Auto-commit at 2023-05-15 09:59:02

--- Code/String/StringExpansion.java
+++ Code/String/StringExpansion.java
@@ -1,2 +1,16 @@
-package String;public class StringExpansion {
+package String;
+import java.util.*;
+//a3b2s1 = aaabbs
+public class StringExpansion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                for(int j=0;j<Integer.parseInt(s.charAt(i)+"");j++){
+                    System.out.print(s.charAt(i-1));
+                }
+            }
+        }
+    }
 }
