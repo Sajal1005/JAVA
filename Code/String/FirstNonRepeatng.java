9fcde789e997c04864863dbbd72b7919c79b7c6c
Sajal1005 <nandasajal.208@gmail.com>
2023-05-16 09:21:05 +0530

    Auto-commit at 2023-05-16 09:21:03

--- Code/String/FirstNonRepeatng.java
+++ Code/String/FirstNonRepeatng.java
@@ -1,2 +1,22 @@
-package String;public class FirstNonRepeatng {
+package String;
+
+import java.util.Scanner;
+
+public class FirstNonRepeatng {
+    public static void main(String[] args) {
+        Scanner in  =new Scanner(System.in);
+        String s = in.nextLine();
+        for(int i=0;i<s.length();i++){
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j)) {
+                    count++;
+                }
+            }
+            if(count==1){
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+    }
 }
