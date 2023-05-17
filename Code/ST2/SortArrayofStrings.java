06e9ff02831be80d7198ca5219ae562bd7f3871d
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:20:05 +0530

    Auto-commit at 2023-05-17 08:20:03

--- Code/ST2/SortArrayofStrings.java
+++ Code/ST2/SortArrayofStrings.java
@@ -4,7 +4,16 @@ import java.util.*;
 public class SortArrayofStrings {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int 
+        int n = in.nextInt();
+        in.nextLine();
+        String[] s = new String[n];
+        for(int i=0;i<n;i++){
+            s[i]=in.nextLine();
+        }
+        Arrays.sort(s);
+        for(int i=0;i<n;i++){
+            System.out.print(s[i]);
+        }
     }
 
 }
