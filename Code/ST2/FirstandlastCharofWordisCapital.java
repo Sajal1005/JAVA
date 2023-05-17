5d85aad9bc5c5ec1a2d7c8106ef4e61f234cc7d1
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:49:03 +0530

    Auto-commit at 2023-05-17 08:49:02

--- Code/ST2/FirstandlastCharofWordisCapital.java
+++ Code/ST2/FirstandlastCharofWordisCapital.java
@@ -9,7 +9,19 @@ public class FirstandlastCharofWordisCapital {
         Scanner in = new Scanner(System.in);
         String s=in.nextLine();
         s=s.toLowerCase();
-        StringBuffer s = new StringBuffer(in.nextLine());
-        for(int )
+        StringBuffer str = new StringBuffer(s);
+        if(str.charAt(0)>='a' && str.charAt(0)<='z'){
+            str.setCharAt(0,(char)(str.charAt(0)-32));
+        }
+        if(str.charAt(str.length()-1)>='a' && str.charAt(str.length()-1)<='z'){
+            str.setCharAt(str.length()-1,(char)(str.charAt(str.length()-1)-32));
+        }
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                if(str.charAt(i+1)>='a' && str.charAt(i+1)<='z'){
+                    str.setCharAt(i+1,(char)(str.charAt(i+1)-32));
+                }
+            }
+        }
     }
 }
