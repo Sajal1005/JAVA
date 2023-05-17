5f9752bec57e8e56447ecf8588dff0705a153b8c
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 07:31:06 +0530

    Auto-commit at 2023-05-17 07:31:03

--- Code/ST2/Palindrome.java
+++ Code/ST2/Palindrome.java
@@ -1,2 +1,24 @@
-package ST2;public class Palindrome {
+package ST2;
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s= in.nextLine();
+        int i=0,j=s.length()-1;
+        int flag=1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                flag=0;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(flag==1){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
 }
