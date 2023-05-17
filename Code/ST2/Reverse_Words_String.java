f79d8a98997abe6434125a88260e3fe82d9ea051
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:15:05 +0530

    Auto-commit at 2023-05-17 08:15:03

--- Code/ST2/Reverse_Words_String.java
+++ Code/ST2/Reverse_Words_String.java
@@ -1,4 +1,14 @@
 package ST2;
+import java.util.*;
 
 public class Reverse_Words_String {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String[] str = s.split(" ");
+        for(int i=0;i<str.length;i++){
+            StringBuffer word = new StringBuffer(str[i]);
+            System.out.println(word.reverse());
+        }
+    }
 }
