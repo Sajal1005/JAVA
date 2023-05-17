d30b0d3dfa3c9eb730fe1705c92becb8d3089e4f
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:42:05 +0530

    Auto-commit at 2023-05-17 08:42:03

--- Code/ST2/FirstandlastCharofWordisCapital.java
+++ Code/ST2/FirstandlastCharofWordisCapital.java
@@ -1,4 +1,13 @@
 package ST2;
+import java.util.*;
+
+// WELCOME TO PUNJAB = WelcomE TO PunjaB
+// wElcOme tO PunJAB = WelcomE TO PunjaB
 
 public class FirstandlastCharofWordisCapital {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in)
+        StringBuffer s = new StringBuffer(in.nextLine());
+        for(int )
+    }
 }
