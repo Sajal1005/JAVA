6c3c12184ae46aae9d9479ec579c3f332b8c1a92
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 12:41:05 +0530

    Auto-commit at 2023-05-17 12:41:03

--- Code/ST2/Panagram.java
+++ Code/ST2/Panagram.java
@@ -5,6 +5,17 @@ public class Panagram {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-        
+        Set<Character> set = new HashSet<>();
+        s=s.toLowerCase();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                set.add(s.charAt(i));
+            }
+        }
+        if(set.size()==26){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
     }
 }
