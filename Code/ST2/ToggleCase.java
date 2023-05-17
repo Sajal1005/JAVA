efcbdcc076f9ba96f894476d6415e1b68c4df2b3
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 07:37:05 +0530

    Auto-commit at 2023-05-17 07:37:03

--- Code/ST2/ToggleCase.java
+++ Code/ST2/ToggleCase.java
@@ -1,4 +1,17 @@
 package ST2;
+import java.util.*;
 
 public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuffer s = new StringBuffer(in.nextLine());
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                s.setCharAt(i,(char)(s.charAt(i)-32));
+            }else if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                s.setCharAt(i,(char)(s.charAt(i)+32));
+            }
+        }
+        System.out.println(s);
+    }
 }
