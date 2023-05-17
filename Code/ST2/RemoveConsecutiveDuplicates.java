5aa6bbc155ff574ddf1e8277652abe1ec87fbf80
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 07:51:05 +0530

    Auto-commit at 2023-05-17 07:51:03

--- Code/ST2/RemoveConsecutiveDuplicates.java
+++ Code/ST2/RemoveConsecutiveDuplicates.java
@@ -1,2 +1,16 @@
-package ST2;public class RemoveConsecutiveDuplicates {
+package ST2;
+import java.util.*;
+
+public class RemoveConsecutiveDuplicates {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuffer s = new StringBuffer(in.nextLine());
+        for(int i=0;i<s.length();i++){
+            if(i+1<s.length() && s.charAt(i)==s.charAt(i+1)){
+                s.deleteCharAt(i+1);
+                i--;
+            }
+        }
+        System.out.println(s);
+    }
 }
