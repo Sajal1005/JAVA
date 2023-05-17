c4eb3ba4c7f40341cb4ebcf3adfd08f80d24be7a
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:38:05 +0530

    Auto-commit at 2023-05-17 08:38:03

--- Code/ST2/Reverse_Vowels_String.java
+++ Code/ST2/Reverse_Vowels_String.java
@@ -1,4 +1,28 @@
 package ST2;
+import java.util.*;
 
 public class Reverse_Vowels_String {
+    public static boolean vowel(char c){
+        if(c=='A' || c=='e' || c=='i' || c=='o' || c=='u' || c=='a' || c=='E' || c=='I' || c=='O' || c=='U'){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuffer s = new StringBuffer(in.nextLine());
+        String vow="";
+        for(int i=0;i<s.length();i++){
+            if(vowel(s.charAt(i))){
+                vow+=s.charAt(i);
+            }
+        }
+        int j=vow.length()-1;
+        for(int i=0;i<s.length();i++){
+            if(vowel(s.charAt(i))){
+                s.setCharAt(i,vow.charAt(j--));
+            }
+        }
+        System.out.println(s);
+    }
 }
