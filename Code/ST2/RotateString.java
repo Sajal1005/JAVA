3ab3c0367b93ed2449dca16efc3f0b67dbffacf6
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 07:45:06 +0530

    Auto-commit at 2023-05-17 07:45:03

--- Code/ST2/RotateString.java
+++ Code/ST2/RotateString.java
@@ -2,9 +2,19 @@ package ST2;
 import java.util.*;
 
 public class RotateString {
+    public static String rotate(String s){
+        String ans="";
+        ans+=s.charAt(s.length()-1);
+        ans+=(s.substring(1));
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-        
+        int n = in.nextInt();
+        for(int i=0;i<n;i++){
+            s=rotate(s);
+        }
+        System.out.println(s);
     }
 }
