e1b41a04402f38bbc3e747c88605d694f65cccea
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 08:25:04 +0530

    Auto-commit at 2023-05-17 08:25:02

--- Code/ST2/SumOfDigitsMapit.java
+++ Code/ST2/SumOfDigitsMapit.java
@@ -1,4 +1,20 @@
 package ST2;
-
+import java.util.*;
+//a12c12d2 = 26
 public class SumOfDigitsMapit {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                int k=i;
+                while(i<s.length() && (s.charAt(i)>='0' && s.charAt(i)<='9')){
+                    i++;
+                }
+                sum+=(Integer.parseInt(s.substring(k,i)+""));
+            }
+        }
+        System.out.println(sum);
+    }
 }
