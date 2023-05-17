67e97a4a3d092d3b0bb84ee3abd3d3e56d9a95a6
Sajal1005 <nandasajal.208@gmail.com>
2023-05-17 11:12:06 +0530

    Auto-commit at 2023-05-17 11:12:03

--- Code/ST2/SentenceToWords.java
+++ Code/ST2/SentenceToWords.java
@@ -1,9 +1,25 @@
 package ST2;
 import java.util.*;
-//IAmACompetitiveProgrammer
-//
+// IAmACompetitiveProgrammer
+// I
+//Am
+//A
+//Competitive
+//Programmer
+
 public class SentenceToWords {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        String ans=s.charAt(0)+"";
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                System.out.println(ans);
+                ans=s.charAt(i)+"";
+            }else{
+                ans+=s.charAt(i);
+            }
+        }
+        System.out.println(ans);
     }
 }
