a74be209c871b630360573b62b096ab13e300398
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 09:23:06 +0530

    Auto-commit at 2023-05-18 09:23:04

--- Code/ST2/Final.java
+++ Code/ST2/Final.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Final {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-//        String s = in.nextLine();
+        String s = in.nextLine();
 
 //        Palindrome
 //        int i=0,j=s.length()-1,flag=1;
@@ -38,6 +38,16 @@ public class Final {
 //        }
 
 
-        
+//        Longest Word in Sentence
+        String[] str = s.split(" ");
+        int ans=0;
+        String st = "";
+        for(int i=0;i<str.length;i++){
+            if(str[i].length()>ans){
+                ans=str[i].length();
+                st=str[i];
+            }
+        }
+        System.out.println(st);
     }
 }
