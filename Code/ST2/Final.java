982400833ec5c673e0c08e10f38d1b8a431140be
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 10:05:04 +0530

    Auto-commit at 2023-05-18 10:05:03

--- Code/ST2/Final.java
+++ Code/ST2/Final.java
@@ -97,20 +97,22 @@ public class Final {
 //        }
 
 //        Find The Most Common Character
-        int ans=0;
-        char c='#';
-        for(int i=0;i<s.length();i++){
-            int count=0;
-            for(int j=0;j<s.length();j++){
-                if(s.charAt(i)==s.charAt(j)){
-                    count++;
-                }
-                if(count>ans){
-                    ans=count;
-                    c=s.charAt(i);
-                }
-            }
-        }
-        System.out.println(c);
+//        int ans=0;
+//        char c='#';
+//        for(int i=0;i<s.length();i++){
+//            int count=0;
+//            for(int j=0;j<s.length();j++){
+//                if(s.charAt(i)==s.charAt(j)){
+//                    count++;
+//                }
+//                if(count>ans){
+//                    ans=count;
+//                    c=s.charAt(i);
+//                }
+//            }
+//        }
+//        System.out.println(c);
+
+        
     }
 }
