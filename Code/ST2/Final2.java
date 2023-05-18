b4090b11696bd61b4970156ff800133041ad2da6
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 11:00:05 +0530

    Auto-commit at 2023-05-18 11:00:03

--- Code/ST2/Final2.java
+++ Code/ST2/Final2.java
@@ -1,4 +1,23 @@
 package ST2;
+import java.util.*;
 
 public class Final2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+//        First Non Repeating Character
+        for(int i=0;i<s.length();i++){
+            int count=0;
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    count++;
+                }
+            }
+            if(count==1){
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+    }
 }
