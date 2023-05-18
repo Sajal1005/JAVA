025236dd4b8ee89a49dbd995f97bcb55adb17954
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 09:15:06 +0530

    Auto-commit at 2023-05-18 09:15:04

--- Code/ST2/Final.java
+++ Code/ST2/Final.java
@@ -5,18 +5,22 @@ public class Final {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-        int i=0,j=s.length()-1,flag=1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                System.out.println(false);
-                flag=0;
-                break;
-            }
-            i++;
-            j--;
-        }
-        if(flag==1){
-            System.out.println(true);
-        }
+
+//        Palindrome
+//        int i=0,j=s.length()-1,flag=1;
+//        while(i<j){
+//            if(s.charAt(i)!=s.charAt(j)){
+//                System.out.println(false);
+//                flag=0;
+//                break;
+//            }
+//            i++;
+//            j--;
+//        }
+//        if(flag==1){
+//            System.out.println(true);
+//        }
+
+        
     }
 }
