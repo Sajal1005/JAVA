4398a7369602c8a0d5c79c6cbbb704b4f0885a15
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 11:09:04 +0530

    Auto-commit at 2023-05-18 11:09:02

--- Code/ST2/Final2.java
+++ Code/ST2/Final2.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Final2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
+        String st = in.nextLine();
 
 //        First Non Repeating Character
 //        for(int i=0;i<s.length();i++){
@@ -20,23 +20,39 @@ public class Final2 {
 //            }
 //        }
 
+
 //        Trim Spaces
 //        s=s.trim();
 //        System.out.println(s);
 
+
+
 //        Adjacent pairs remove small
-        int i=0,j=1;
-        while(i<s.length() && j<s.length()){
-            if(s.charAt(i)<s.charAt(j)){
-                System.out.print(s.charAt(j));
-            }else{
-                System.out.print(s.charAt(i));
+//        int i=0,j=1;
+//        while(i<s.length() && j<s.length()){
+//            if(s.charAt(i)<s.charAt(j)){
+//                System.out.print(s.charAt(j));
+//            }else{
+//                System.out.print(s.charAt(i));
+//            }
+//            i+=2;
+//            j+=2;
+//        }
+//        if(s.length()%2!=0){
+//            System.out.print(s.charAt(s.length()-1));
+//        }
+
+
+        st=st.toLowerCase();
+        StringBuffer s = new StringBuffer(st);
+        s.setCharAt(0,(char)(s.charAt(0)-32));
+        s.setCharAt(s.length()-1,(char)(s.charAt(s.length()-1)-32));
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                s.setCharAt(i-1,(char)(s.charAt(i-1)-32));
+                s.setCharAt(i+1,(char)(s.charAt(i+1)-32));
             }
-            i+=2;
-            j+=2;
         }
-        if(s.length()%2!=0){
-            System.out.print(s.charAt(s.length()-1));
-        }
+        System.out.println(s);
     }
 }
