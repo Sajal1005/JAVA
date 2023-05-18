ecea1385730de3ee618224cf67c69aa8d3e9551b
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 11:19:03 +0530

    Auto-commit at 2023-05-18 11:19:02

--- Code/ST2/Final2.java
+++ Code/ST2/Final2.java
@@ -67,16 +67,30 @@ public class Final2 {
 
 
 //      number of data characters that do not change position even after the data stream is reversed
-        String rev= "";
-        for(int i=s.length()-1;i>=0;i--){
-            rev+=s.charAt(i);
-        }
-        int count=0;
+//        String rev= "";
+//        for(int i=s.length()-1;i>=0;i--){
+//            rev+=s.charAt(i);
+//        }
+//        int count=0;
+//        for(int i=0;i<s.length();i++){
+//            if(s.charAt(i)==rev.charAt(i)){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+
+//        Sum of numbers between strings
+        int sum=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==rev.charAt(i)){
-                count++;
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                int k=i;
+                while(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                    i++;
+                }
+                sum+=(Integer.parseInt(s.substring(k,i)+""));
             }
         }
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
