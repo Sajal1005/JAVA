e10cad46b0b41c423ca155fda928f69ad2ee03e8
Sajal1005 <nandasajal.208@gmail.com>
2023-05-18 09:14:05 +0530

    Auto-commit at 2023-05-18 09:14:03

--- Code/ST2/Final.java
+++ Code/ST2/Final.java
@@ -1,2 +1,22 @@
-package ST2;public class Final {
+package ST2;
+import java.util.*;
+
+public class Final {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int i=0,j=s.length()-1,flag=1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                System.out.println(false);
+                flag=0;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(flag==1){
+            System.out.println(true);
+        }
+    }
 }
