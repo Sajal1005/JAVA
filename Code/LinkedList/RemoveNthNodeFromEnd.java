5648fba3bfdb4904e4fc6e3462092f34d8549a73
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 18:56:03 +0530

    Auto-commit at 2023-05-20 18:56:02

--- Code/LinkedList/RemoveNthNodeFromEnd.java
+++ Code/LinkedList/RemoveNthNodeFromEnd.java
@@ -4,22 +4,35 @@ public class RemoveNthNodeFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
 //      Brute-Force
-        ListNode temp = head;
-        int count=0;
-        while(temp!=null){
-            temp=temp.next;
-            count++;
-        }
-        if(count==n){return head.next;}
-        count-=(n+1);
-        temp=head;
-        for(int i=0;i<count;i++){
-            temp=temp.next;
-        }
-        temp.next=temp.next.next;
-        return head;
+//        ListNode temp = head;
+//        int count=0;
+//        while(temp!=null){
+//            temp=temp.next;
+//            count++;
+//        }
+//        if(count==n){return head.next;}
+//        count-=(n+1);
+//        temp=head;
+//        for(int i=0;i<count;i++){
+//            temp=temp.next;
+//        }
+//        temp.next=temp.next.next;
+//        return head;
 
-//      Slow-Fast Pointers
         
+//      Slow-Fast Pointers
+        ListNode fast=head,slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return head.next;
+        }
+        while(fast!=null &&  fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
     }
 }
