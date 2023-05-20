dbd3faa53ff047bda02782a0a5123c01cbb0ca04
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 18:23:02 +0530

    Auto-commit at 2023-05-20 18:23:01

--- Code/LinkedList/RemoveCycle.java
+++ Code/LinkedList/RemoveCycle.java
@@ -1,5 +1,31 @@
 package LinkedList;
 
 public class RemoveCycle {
-     
+    public ListNode removeCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        int flag=0;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            return null;
+        }
+        if(slow==head){
+            while(fast.next!=slow){
+                fast=fast.next;
+            }
+        }else{
+            slow=head;
+            while(slow.next!=fast.next){
+                slow=slow.next;
+                fast=fast.next;
+            }
+        }
+        fast.next=null;
+    }
 }
