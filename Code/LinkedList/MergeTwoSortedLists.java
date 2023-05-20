6a7eb1508a76d67ca859d7680a638d7d70e4d0a1
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 16:51:05 +0530

    Auto-commit at 2023-05-20 16:51:03

--- Code/LinkedList/MergeTwoSortedLists.java
+++ Code/LinkedList/MergeTwoSortedLists.java
@@ -1,5 +1,13 @@
 package LinkedList;
 
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode ans = new ListNode(-1);
