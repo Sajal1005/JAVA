6568c7ff4d83c2a75b42b15d71607b2a74306922
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 18:43:03 +0530

    Auto-commit at 2023-05-20 18:43:02

--- Code/LinkedList/RemoveNthNodeFromEnd.java
+++ Code/LinkedList/RemoveNthNodeFromEnd.java
@@ -1,18 +1,25 @@
 package LinkedList;
 
 public class RemoveNthNodeFromEnd {
-    ListNode temp = head;
-    int count=0;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+//      Brute-Force
+        ListNode temp = head;
+        int count=0;
         while(temp!=null){
-        temp=temp.next;
-        count++;
-    }
+            temp=temp.next;
+            count++;
+        }
         if(count==n){return head.next;}
-    count-=(n+1);
-    temp=head;
+        count-=(n+1);
+        temp=head;
         for(int i=0;i<count;i++){
-        temp=temp.next;
-    }
-    temp.next=temp.next.next;
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
         return head;
+
+//      Slow-Fast Pointers
+        
+    }
 }
