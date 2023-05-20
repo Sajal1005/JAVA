2ff2c468454a48fa8869916b5d3b240bae2bae5f
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 16:54:04 +0530

    Auto-commit at 2023-05-20 16:54:02

--- Code/LinkedList/ReverseLinkedList.java
+++ Code/LinkedList/ReverseLinkedList.java
@@ -1,5 +1,14 @@
 package LinkedList;
 
 public class ReverseLinkedList {
-
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null,temp=null,curr=head;
+        while(curr!=null){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
 }
