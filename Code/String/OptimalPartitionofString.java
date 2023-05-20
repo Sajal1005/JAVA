f71366149acd0b8ad5c8b4038d5f0f4edda13d46
Sajal1005 <nandasajal.208@gmail.com>
2023-05-20 16:31:05 +0530

    Auto-commit at 2023-05-20 16:31:03

--- Code/String/OptimalPartitionofString.java
+++ Code/String/OptimalPartitionofString.java
@@ -1,5 +1,19 @@
 package String;
+import java.util.*;
 
 public class OptimalPartitionofString {
-    
+    public int partitionString(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(!set.contains(s.charAt(i))){
+                set.add(s.charAt(i));
+            }else{
+                count++;
+                set.clear();
+                i--;
+            }
+        }
+        return count+1;
+    }
 }
