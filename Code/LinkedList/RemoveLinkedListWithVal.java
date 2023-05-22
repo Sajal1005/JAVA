31cccff5e3d9e7adea998d17a6c8265d420d5c9e
Sajal1005 <nandasajal.208@gmail.com>
2023-05-22 11:49:06 +0530

    Auto-commit at 2023-05-22 11:49:04

--- Code/LinkedList/RemoveLinkedListWithVal.java
+++ Code/LinkedList/RemoveLinkedListWithVal.java
@@ -1,4 +1,17 @@
 package LinkedList;
 
 public class RemoveLinkedListWithVal {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode ans = new ListNode(-1);
+        ans.next=head;
+        ListNode temp=ans;
+        while(temp!=null && temp.next!=null){
+            if(val==temp.next.val){
+                temp.next=temp.next.next;
+            }else{
+                temp=temp.next;
+            }
+        }
+        return ans.next;
+    }
 }
