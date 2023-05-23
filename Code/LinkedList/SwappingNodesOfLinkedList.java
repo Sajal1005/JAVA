cb2011e5642db91f4d3eec10391feeba798fdb7f
Sajal1005 <nandasajal.208@gmail.com>
2023-05-23 09:24:03 +0530

    Auto-commit at 2023-05-23 09:24:02

--- Code/LinkedList/SwappingNodesOfLinkedList.java
+++ Code/LinkedList/SwappingNodesOfLinkedList.java
@@ -1,5 +1,23 @@
 package LinkedList;
 
 public class SwappingNodesOfLinkedList {
-    
+    public ListNode swapNodes(ListNode head, int k) {
+        int n=k,count=0;
+        ListNode temp=head,temp2=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        temp=head;
+        while((--k)>0){
+            temp=temp.next;
+        }
+        for(int i=0;i<count-n;i++){
+            temp2=temp2.next;
+        }
+        int t = temp.val;
+        temp.val=temp2.val;
+        temp2.val=t;
+        return head;
+    }
 }
