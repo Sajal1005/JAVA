914e7b75d101d435a57d18c7d05abcbbf8a1fa72
Sajal1005 <nandasajal.208@gmail.com>
2023-05-23 09:42:06 +0530

    Auto-commit at 2023-05-23 09:42:03

--- Code/LinkedList/MaximumTwinSum.java
+++ Code/LinkedList/MaximumTwinSum.java
@@ -1,4 +1,28 @@
 package LinkedList;
 
 public class MaximumTwinSum {
+    public int pairSum(ListNode head) {
+        ListNode slow=head,fast=head,prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
+        ListNode curr=slow,temp=null;
+        prev=null;
+        while(curr!=null){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        int ans=0;
+        while(head!=null && prev!=null){
+            ans=Math.max(ans,head.val+prev.val);
+            head=head.next;
+            prev=prev.next;
+        }
+        return ans;
+    }
 }
