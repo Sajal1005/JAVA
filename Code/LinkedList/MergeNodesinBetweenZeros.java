27828009e884faedbaf8c8e9fda2ad99d9d52359
Sajal1005 <nandasajal.208@gmail.com>
2023-05-25 14:02:02 +0530

    Auto-commit at 2023-05-25 14:02:02

--- Code/LinkedList/MergeNodesinBetweenZeros.java
+++ Code/LinkedList/MergeNodesinBetweenZeros.java
@@ -1,5 +1,23 @@
 package LinkedList;
 
 public class MergeNodesinBetweenZeros {
-    
+    public ListNode mergeNodes(ListNode head) {
+        ListNode ans = new ListNode(-1);
+        ListNode tail=ans;
+        ListNode temp = head;
+        int sum=0;
+        temp=temp.next;
+        while(temp!=null){
+            if(temp.val==0){
+                ListNode newn = new ListNode(sum);
+                tail.next=newn;
+                tail=tail.next;
+                sum=0;
+            }else{
+                sum+=temp.val;
+            }
+            temp=temp.next;
+        }
+        return ans.next;
+    }
 }
