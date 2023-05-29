06c433895f8f14aaf245231ad25c3a6493e0ee92
Sajal1005 <nandasajal.208@gmail.com>
2023-05-29 16:22:04 +0530

    Auto-commit at 2023-05-29 16:22:02

--- Code/Sort.java
+++ Code/Sort.java
@@ -3,23 +3,10 @@ import java.util.*;
 public class Sort {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=in.nextInt();
-        }
-//        for(int i=0;i<n-1;i++){
-//            for(int j=0;j<n-i-1;j++){
-//                if(arr[j]>arr[j+1]){
-//                    int temp=arr[j];
-//                    arr[j]=arr[j+1];
-//                    arr[j+1]=temp;
-//                }
-//            }
-//        }
-        Arrays.sort(arr);
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+        String s= in.nextLine();
+        String str[] = s.split(" ");
+        for(int i=0;i<str.length;i++){
+
         }
     }
 }
