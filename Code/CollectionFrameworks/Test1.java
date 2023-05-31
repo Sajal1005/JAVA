b1516404ca23afe8e16a3b9c1e6c4ab249691eb0
Sajal1005 <nandasajal.208@gmail.com>
2023-05-31 11:14:03 +0530

    Auto-commit at 2023-05-31 11:14:02

--- Code/CollectionFrameworks/Test1.java
+++ Code/CollectionFrameworks/Test1.java
@@ -1,7 +1,16 @@
 package CollectionFrameworks;
 
+import java.util.*;
+
 public class Test1 {
     public static void main(String[] args) {
-        
+       Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+       HashSet<String> s = new HashSet<>();
+       for(int i=0;i<n;i++){
+           String str = in.next();
+           s.add(str);
+       }
+        System.out.println(s.size());
     }
 }
