0320aa42b97550d2c3fa1977b901de9443089ef9
Sajal1005 <nandasajal.208@gmail.com>
2023-05-31 11:38:03 +0530

    Auto-commit at 2023-05-31 11:38:02

--- Code/CollectionFrameworks/Test3.java
+++ Code/CollectionFrameworks/Test3.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Test3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            String s = in.next();
+            ans=Math.max(s.length(),ans);
+        }
+        System.out.println(ans);
     }
 }
