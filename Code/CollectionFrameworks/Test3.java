ad225099365048d11058d1db4aed34f5a321e1d2
Sajal1005 <nandasajal.208@gmail.com>
2023-05-31 11:36:03 +0530

    Auto-commit at 2023-05-31 11:36:02

--- Code/CollectionFrameworks/Test3.java
+++ Code/CollectionFrameworks/Test3.java
@@ -1,2 +1,10 @@
-package CollectionFrameworks;public class Test3 {
+package CollectionFrameworks;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
 }
