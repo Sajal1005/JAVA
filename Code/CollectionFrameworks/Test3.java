c45e74b893c08a46959d19ce6b6af96c807d4f19
Sajal1005 <nandasajal.208@gmail.com>
2023-05-31 11:54:03 +0530

    Auto-commit at 2023-05-31 11:54:02

--- Code/CollectionFrameworks/Test3.java
+++ Code/CollectionFrameworks/Test3.java
@@ -1,5 +1,6 @@
 package CollectionFrameworks;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test3 {
@@ -7,9 +8,13 @@ public class Test3 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int ans=0;
+        ArrayList<String> str = new ArrayList<>();
         for(int i=0;i<n;i++){
             String s = in.next();
-            ans=Math.max(s.length(),ans);
+            str.add(s);
+        }
+        for(int i=0;i<n;i++){
+            ans=Math.max(str.get(i).length(),ans);
         }
         System.out.println(ans);
     }
