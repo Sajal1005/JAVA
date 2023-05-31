283dd0d0ab108980e23f90d6f65291866c83074a
Sajal1005 <nandasajal.208@gmail.com>
2023-05-31 11:17:03 +0530

    Auto-commit at 2023-05-31 11:17:02

--- Code/CollectionFrameworks/Test2.java
+++ Code/CollectionFrameworks/Test2.java
@@ -5,6 +5,11 @@ import java.util.*;
 public class Test2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            min=Math.min(min,in.nextInt());
+        }
+        System.out.println(min);
     }
 }
