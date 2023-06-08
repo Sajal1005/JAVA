f628ad91d06085c373844dfbc03375c58c90280f
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 07:39:06 +0530

    Auto-commit at 2023-06-08 07:39:03

--- Code/ETE.java
+++ Code/ETE.java
@@ -169,14 +169,23 @@ class ETE{
 //        }
 //        System.out.println(ans);
 
+//        int n = in.nextInt();
+//        int arr[] = new int[n];
+//        int count=0;
+//        for(int i=0;i<n;i++){
+//            arr[i]=in.nextInt();
+//            if(cube(arr[i])){
+//                System.out.print(arr[i]+" ");
+//            }
+//        }
+
         int n = in.nextInt();
-        int arr[] = new int[n];
-        int count=0;
+        int a=0,b=1,temp;
         for(int i=0;i<n;i++){
-            arr[i]=in.nextInt();
-            if(cube(arr[i])){
-                System.out.print(arr[i]+" ");
-            }
+            System.out.print(a+" ");
+            temp=b;
+            b+=a;
+            a=temp;
         }
 
     }
