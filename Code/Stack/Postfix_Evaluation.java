c512e04b82e703e7bda7280a13c71e39f940837a
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 16:06:04 +0530

    Auto-commit at 2023-06-08 16:06:03

--- Code/Stack/Postfix_Evaluation.java
+++ Code/Stack/Postfix_Evaluation.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Postfix_Evaluation {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String s = in.next();
         
     }
 }
