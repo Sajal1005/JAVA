f21c565ed9be82953750148bacc62d9e8d2be818
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 16:36:05 +0530

    Auto-commit at 2023-06-08 16:36:03

--- Code/Stack/Balanced_Parenthesis.java
+++ Code/Stack/Balanced_Parenthesis.java
@@ -1,4 +1,20 @@
 package Stack;
+import java.util.*;
 
 public class Balanced_Parenthesis {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Stack<Character> st = new Stack<>();
+        String s = in.next();
+        for(int i=0;i<s.length();i++){
+            if(st.empty()){
+                st.push(s.charAt(i));
+            }else if(st.peek()=='(' && s.charAt(i)==')'){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        System.out.println(st.size());
+    }
 }
