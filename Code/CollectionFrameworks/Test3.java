4110499151cfddade6bc52eb3c260c624c3ee560
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 06:47:05 +0530

    Auto-commit at 2023-06-08 06:47:03

--- Code/CollectionFrameworks/Test3.java
+++ Code/CollectionFrameworks/Test3.java
@@ -7,15 +7,18 @@ public class Test3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int ans=0;
         ArrayList<String> str = new ArrayList<>();
         for(int i=0;i<n;i++){
             String s = in.next();
             str.add(s);
         }
-        for(int i=0;i<n;i++){
-            ans=Math.max(str.get(i).length(),ans);
+        String s =in.next();
+        for(int i=0;i<str.size();i++){
+            if(str.get(i).compareTo(s)<=0){
+                str.remove(i);
+                i--;
+            }
         }
-        System.out.println(ans);
+        System.out.println(str);
     }
 }
