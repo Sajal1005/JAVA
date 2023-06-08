11b3d21063c05d3152b5bebe462de58a748bb9cf
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 06:57:07 +0530

    Auto-commit at 2023-06-08 06:57:04

--- Code/ETE.java
+++ Code/ETE.java
@@ -4,21 +4,47 @@ class ETE{
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-        int ans=0;
+//        int ans=0;
+//        int n = in.nextInt();
+//        int[] arr = new int[n];
+//        for(int i=0;i<n;i++){
+//            ans*=10;
+//            arr[i]=in.nextInt();
+//            ans+=arr[i];
+//        }
+//        ans+=1;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        while(ans!=0){
+//            list.add(ans%10);
+//            ans/=10;
+//        }
+//        Collections.reverse(list);
+//        System.out.println(list);
+        // 5 1 2 3 8 10 11 12 14
+//Add only continuity sum like 1 2 3....then break at 8 .....then again sum 10 11 12
+
+//6
+//1 2 3 5 7 8
+//21 ans
         int n = in.nextInt();
-        int[] arr = new int[n];
+        int arr[] = new int[n];
         for(int i=0;i<n;i++){
-            ans*=10;
             arr[i]=in.nextInt();
-            ans+=arr[i];
         }
-        ans+=1;
-        ArrayList<Integer> list = new ArrayList<>();
-        while(ans!=0){
-            list.add(ans%10);
-            ans/=10;
+        int ans=0;
+        int flag=0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]+1==arr[i+1]){
+                if(flag==0){
+                    ans+=arr[i]+arr[i+1];
+                    flag=1;
+                }else{
+                    ans+=arr[i+1];
+                }
+            }else{
+                flag=0;
+            }
         }
-        Collections.reverse(list);
-        System.out.println(list);
+        System.out.println(ans);
     }
 }
