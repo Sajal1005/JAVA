130a029828dad1f19208c6a5baaf13270d6d88ac
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 22:14:05 +0530

    Auto-commit at 2023-06-08 22:14:03

--- Code/ETE.java
+++ Code/ETE.java
@@ -795,22 +795,35 @@ class ETE{
 
 
         int n = in.nextInt();
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print(".");
+//            }for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print(".");
+//            }
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        int arr[] = new int[n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                System.out.print(".");
-            }for(int j=0;j<i+1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+            arr[i]=in.nextInt();
         }
+        int ans=Integer.MIN_VALUE,sum=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                System.out.print(".");
+            if(sum<0){
+                sum=0;
             }
-            for(int j=0;j<n-i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+            sum+=arr[i];
+            ans=Math.max(sum,ans);
         }
+        System.out.println(sum);
     }
 }
