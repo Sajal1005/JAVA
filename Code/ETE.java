33f9d770932e78790c2a8cd4091574f1d2e19cfc
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 11:56:05 +0530

    Auto-commit at 2023-06-08 11:56:03

--- Code/ETE.java
+++ Code/ETE.java
@@ -444,24 +444,26 @@ class ETE{
 //            }
 //        }
 
-        int n = in.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                System.out.print(".");
-            }
-            for(int j=0;j<i+1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                System.out.print(".");
-            }
-            for(int j=0;j<n-i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        int n = in.nextInt();
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print(".");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print(".");
+//            }
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        
     }
 }
