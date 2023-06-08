e734d5e50bab53c6744711f77417092b7082e1fd
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 06:51:05 +0530

    Auto-commit at 2023-06-08 06:51:03

--- Code/ETE.java
+++ Code/ETE.java
@@ -3,5 +3,21 @@ import java.util.*;
 class ETE{
     public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+        int ans=0;
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            ans*=10;
+            arr[i]=in.nextInt();
+            ans+=arr[i];
+        }
+        ans+=1;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(ans!=0){
+            list.add(ans%10);
+            ans/=10;
+        }
+        System.out.println(list);
     }
 }
