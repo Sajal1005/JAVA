454485dcd37c4a3d62e4d8f40486ac860f2ae802
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 17:02:04 +0530

    Auto-commit at 2023-06-08 17:02:03

--- Code/Stack/StockSpan.java
+++ Code/Stack/StockSpan.java
@@ -1,7 +1,24 @@
 package Stack;
+import java.util.*;
 
 public class StockSpan {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int count = 1;
+        System.out.print(1 + " ");
+        for (int i = 0; i < n - 1; i++){
+            if (arr[i + 1] > arr[i]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            System.out.print(count+" ");
+        }
+        System.out.print("END");
     }
 }
