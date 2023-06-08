25bf7f3927b6276222b57f90a5d99433f806a3cc
Sajal1005 <nandasajal.208@gmail.com>
2023-06-08 07:23:06 +0530

    Auto-commit at 2023-06-08 07:23:03

--- Code/ETE.java
+++ Code/ETE.java
@@ -128,7 +128,15 @@ class ETE{
         int k = in.nextInt();
         int arr[] = new int[n];
         for(int i=0;i<n;i++){
-            
+            arr[i] = in.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==k){
+                    System.out.println(i+" "+j);
+                    System.exit(0);
+                }
+            }
         }
     }
 }
