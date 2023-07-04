96a4f7ad91049d45e595af69abdce66672540bf8
Sajal1005 <nandasajal.208@gmail.com>
2023-07-04 18:42:20 +0530

    commit

--- Code/Patterns/Practice.java
+++ Code/Patterns/Practice.java
@@ -38,5 +38,51 @@ public class Practice {
 //            }
 //            System.out.println();
 //        }
+
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                if((i+j)%2==0){
+//                    System.out.print(1);
+//                }else{
+//                    System.out.print(0);
+//                }
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(j+1);
+//            }
+//            for(int j=0;j<(n+2)-2*i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                System.out.print(i-j+1);
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print((char)(65+i));
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-1-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                System.out.print((char)(65+j));
+//            }
+//            for(int j=0;j<i;j++){
+//                System.out.print((char)(64+i-j));
+//            }
+//            System.out.println();
+//        }
+
+
     }
 }
