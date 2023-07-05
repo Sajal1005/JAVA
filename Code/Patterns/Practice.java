b9c55dc7605ca237023caedeceb100f8deb29789
Sajal1005 <nandasajal.208@gmail.com>
2023-07-05 10:11:32 +0530

    commit

--- Code/Patterns/Practice.java
+++ Code/Patterns/Practice.java
@@ -83,6 +83,100 @@ public class Practice {
 //            System.out.println();
 //        }
 
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print((char)(n+64+j-i));
+//            }
+//            System.out.println();
+//        }
 
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            for(int j=0;j<2*i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            for(int j=0;j<2*n-2*i-2;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            for(int j=0;j<2*n-2-2*i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<i+1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n-1;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print("*");
+//            }
+//            for(int j=0;j<2*i+2;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=0;i<2*n-1;i++){
+//            for(int j=0;j<2*n-1;j++){
+//                System.out.print(n-Math.min((Math.min(i,j)),Math.min(2*n-i-2,2*n-j-2)));
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                if(i==j){
+//                    System.out.print(i+1);
+//                }else if(j==n-i-1){
+//                    System.out.print(i+1);
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
+
+
+        for(int i=0;i<=n/2;i++){
+            for(int j=0;j<n/2-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n-n/2-1;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<n-2-2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
     }
 }
