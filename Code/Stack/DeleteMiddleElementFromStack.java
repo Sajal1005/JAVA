a95cb45ae99329e2ee4913bff9417b16ed811412
Sajal1005 <nandasajal.208@gmail.com>
2023-07-09 22:06:07 +0530

    stack

--- Code/Stack/DeleteMiddleElementFromStack.java
+++ Code/Stack/DeleteMiddleElementFromStack.java
@@ -1,5 +1,16 @@
 package Stack;
+import java.util.*;
 
 public class DeleteMiddleElementFromStack {
+    public void delete(Stack<Integer> s, int count,int n){
 
+        if(count==(n/2)){
+            s.pop();
+            return;
+        }
+
+        int num=s.pop();
+        delete(s,count+1,n);
+        s.push(num);
+    }
 }
