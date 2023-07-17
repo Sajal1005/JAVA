1b5eac72d06543807a40ecd54acb18c569c8cf5c
Sajal1005 <nandasajal.208@gmail.com>
2023-07-17 08:55:52 +0530

    bs

---
+++ Code/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class SearchInsertPosition {
+
+    //    EITHER IMPLEMENT LOWER BOUND
+
+    //    OR
+
+    //    return low
+        public int searchInsert(int[] arr, int target) {
+            int low=0,high=arr.length-1;
+            while(low<=high){
+                int mid = low+(high-low)/2;
+                if(arr[mid]==target){
+                    return mid;
+                }else if(arr[mid]<target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+            return low;
+        }
+
+}
